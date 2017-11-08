297cb8805104b9afe2a036699c3d1c895dbd5a7c
John Craddock <jtcraddock@gmail.com>
2017-11-08 13:40:16 +0000

    remove ES6 string template

--- simple-popup.js
+++ simple-popup.js
@@ -396,79 +396,84 @@ Except as contained in this notice, the name of the John Craddock shall not be u
     return frag;
   }
 
-  var svg = `<svg 
-                version="1.1" 
-                xmlns="http://www.w3.org/2000/svg" 
-                xmlns:xlink="http://www.w3.org/1999/xlink" 
-                width="4em"
-                height="4em"
-                x="0px" 
-                y="0px"
-                viewBox="0 0 30 30" 
-                enable-background="new 0 0 30 30" 
-                xml:space="preserve">
-              <g id="icon-background-circle">
-                <ellipse 
-                  transform="matrix(0.7071 -0.7071 0.7071 0.7071 -6.1304 14.8)" 
-                  fill="#FBC830" 
-                  stroke="#FBC830" 
-                  stroke-miterlimit="10" 
-                  cx="14.8" 
-                  cy="14.8" 
-                  rx="13.4" 
-                  ry="13.4"/>
-              </g>
-              <g id="exclamation-mark">
-                <g>
-                  <path 
-                    fill="#FFFFFF" 
-                    d="M13,21.7c0-0.6,0.2-1,0.5-1.3s0.8-0.5,1.4-0.5c0.6,0,1.1,0.2,1.4,0.5c0.4,0.3,0.5,0.8,0.5,1.3
-                    c0,0.6-0.2,1-0.5,1.3c-0.4,0.3-0.8,0.5-1.4,0.5c-0.6,0-1-0.2-1.4-0.5S13,22.3,13,21.7z M13.4,6.5h3.2v7.9L16,18.3h-2l-0.6-3.9V6.5
-                    z"/>
-                </g>
-              </g>
-            </svg>`;
+  // var svg = `<svg 
+  //               version="1.1" 
+  //               xmlns="http://www.w3.org/2000/svg" 
+  //               xmlns:xlink="http://www.w3.org/1999/xlink" 
+  //               width="4em"
+  //               height="4em"
+  //               x="0px" 
+  //               y="0px"
+  //               viewBox="0 0 30 30" 
+  //               enable-background="new 0 0 30 30" 
+  //               xml:space="preserve">
+  //             <g id="icon-background-circle">
+  //               <ellipse 
+  //                 transform="matrix(0.7071 -0.7071 0.7071 0.7071 -6.1304 14.8)" 
+  //                 fill="#FBC830" 
+  //                 stroke="#FBC830" 
+  //                 stroke-miterlimit="10" 
+  //                 cx="14.8" 
+  //                 cy="14.8" 
+  //                 rx="13.4" 
+  //                 ry="13.4"/>
+  //             </g>
+  //             <g id="exclamation-mark">
+  //               <g>
+  //                 <path 
+  //                   fill="#FFFFFF" 
+  //                   d="M13,21.7c0-0.6,0.2-1,0.5-1.3s0.8-0.5,1.4-0.5c0.6,0,1.1,0.2,1.4,0.5c0.4,0.3,0.5,0.8,0.5,1.3
+  //                   c0,0.6-0.2,1-0.5,1.3c-0.4,0.3-0.8,0.5-1.4,0.5c-0.6,0-1-0.2-1.4-0.5S13,22.3,13,21.7z M13.4,6.5h3.2v7.9L16,18.3h-2l-0.6-3.9V6.5
+  //                   z"/>
+  //               </g>
+  //             </g>
+  //           </svg>`;
 
-  var defaultContentStrings = {
-    en: `<section>
-          <div class="splash-title">
-            <h3>Oops! Something Went Wrong</h3>
-          </div> 
-          <div class="splash-white center-text">
-            <p>${svg}</p>
-            <p>If you've been working, don't worry, your progress will be saved.</p>
-            <p>Please reload the page to return to where you were.</p>
-          </div>
-          <div class="splash-footer">
-            <button onclick="window.location.reload()" class="btn-2 center-horizontally">Reload</button>
-          </div>
-        </section>`,
-    es: `<section>
-          <div class="splash-title">
-            <h3>Vaya ¡algo ha salido mal!</h3>
-          </div> 
-          <div class="splash-white center-text">
-            <p>${svg}</p>
-            <p>Si estabas trabajando, no te preocupes, tu progreso se guardará.</p>
-            <p>Vuelve a refrescar la página para regresar a donde estabas.</p>
-          </div>
-          <div class="splash-footer">
-            <button onclick="window.location.reload()" class="btn-2 center-horizontally">Refrescar</button>
-          </div>
-        </section>`,
-    sk: `<section>
-          <div class="splash-title">
-            <h3>Oops! Something Went Wrong</h3>
-          </div> 
-          <div class="splash-white center-text">
-            <p>${svg}</p>
-            <p>If you've been working, don't worry, your progress will be saved.</p>
-            <p>Please reload the page to return to where you were.</p>
-          </div>
-          <div class="splash-footer">
-            <button onclick="window.location.reload()" class="btn-2 center-horizontally">Reload</button>
-          </div>
-        </section>`
-  };
+     var defaultContentStrings ={
+      en: 'error',
+      es: 'error',
+      sk: 'error'
+     };
+  // var defaultContentStrings = {
+  //   en: `<section>
+  //         <div class="splash-title">
+  //           <h3>Oops! Something Went Wrong</h3>
+  //         </div> 
+  //         <div class="splash-white center-text">
+  //           <p>${svg}</p>
+  //           <p>If you've been working, don't worry, your progress will be saved.</p>
+  //           <p>Please reload the page to return to where you were.</p>
+  //         </div>
+  //         <div class="splash-footer">
+  //           <button onclick="window.location.reload()" class="btn-2 center-horizontally">Reload</button>
+  //         </div>
+  //       </section>`,
+  //   es: `<section>
+  //         <div class="splash-title">
+  //           <h3>Vaya ¡algo ha salido mal!</h3>
+  //         </div> 
+  //         <div class="splash-white center-text">
+  //           <p>${svg}</p>
+  //           <p>Si estabas trabajando, no te preocupes, tu progreso se guardará.</p>
+  //           <p>Vuelve a refrescar la página para regresar a donde estabas.</p>
+  //         </div>
+  //         <div class="splash-footer">
+  //           <button onclick="window.location.reload()" class="btn-2 center-horizontally">Refrescar</button>
+  //         </div>
+  //       </section>`,
+  //   sk: `<section>
+  //         <div class="splash-title">
+  //           <h3>Oops! Something Went Wrong</h3>
+  //         </div> 
+  //         <div class="splash-white center-text">
+  //           <p>${svg}</p>
+  //           <p>If you've been working, don't worry, your progress will be saved.</p>
+  //           <p>Please reload the page to return to where you were.</p>
+  //         </div>
+  //         <div class="splash-footer">
+  //           <button onclick="window.location.reload()" class="btn-2 center-horizontally">Reload</button>
+  //         </div>
+  //       </section>`
+  // };
   var defaultContent = defaultContentStrings.en;
 }());
